aebdb906381640b79d26a385d929c199f618c9da
Chris <123502525+ccm89@users.noreply.github.com>
2026-08-18 12:12:01 -0400

    Add: python-default-dict - submission-0

---
+++ Python Coding Interviews/python-default-dict/submission-0.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from typing import List, Dict
+
+
+def count_chars(s: str) -> Dict[str, int]:
+    output = defaultdict(int)
+    for c in s:
+        output[c] += 1
+    return output
+
+
+def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+    output = defaultdict(list)
+    for sublist in nums:
+        for i in range(len(sublist)-1):
+            output[sublist[0]].append(sublist[i+1])
+    return output
+
+
+# do not modify below this line
+print(count_chars("hello"))
+print(count_chars("helloworld"))
+print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+
+print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
