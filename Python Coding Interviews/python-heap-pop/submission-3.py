6d7654cb53037000ef6cfbc282727433c7c0455f
Chris <123502525+ccm89@users.noreply.github.com>
2026-08-19 16:07:08 -0400

    Add: python-heap-pop - submission-3

---
+++ Python Coding Interviews/python-heap-pop/submission-3.py
@@ -0,0 +1,23 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+    # Inital solution
+    # n = len(heap)
+    # res = []
+    # for _ in range(n):
+    #     res.append(heapq.heappop(heap))
+    # return res
+
+    # Solution using comprehension
+    # though heap is resizing len(heap)
+    # uses original length n  before resizing occurs.
+    return [heapq.heappop(heap) for _ in range(len(heap))]
+
+
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
